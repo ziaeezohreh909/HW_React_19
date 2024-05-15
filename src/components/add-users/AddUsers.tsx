a2509cd880f5ddeecfb775df6573c8c9679634e0
“ziaeezohreh909” <ziaeezohreh909@gmail.com>
2024-05-15 13:22:05 +0330

    create components

---
+++ src/components/add-users/AddUsers.tsx
@@ -0,0 +1,8 @@
+
+export default function AddUsers() {
+  return (
+    <div>
+      add user
+    </div>
+  )
+}
---
+++ src/components/users-list/UsersList.tsx
@@ -0,0 +1,8 @@
+
+export default function UsersList() {
+  return (
+    <div>
+      user list
+    </div>
+  )
+}
